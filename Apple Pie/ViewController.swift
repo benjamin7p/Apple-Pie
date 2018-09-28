d99c86d22e6613b28d427242e75a6855bac17c98
Benjamin PRO <benjaminpoulsenpro@benjamins-macbook-air.local>
2018-09-27 20:34:22 -0600

    apple pie buttons

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
